f0a7ff4295a4a5ae65dafde36cbcae71c03024db
enki <enki94@gmail.com>
2019-02-22 09:36:11 +0900

    first commit

---
+++ src/main/java/io/devwin/mychatbot/MychatbotApplication.java
@@ -0,0 +1,13 @@
+package io.devwin.mychatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychatbotApplication.class, args);
+	}
+
+}
